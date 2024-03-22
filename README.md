1fd9e9af200fedd0e29cb6ddf4563c9c7015cfdc
Kwizera Marius Tresor <kwizeramariustresor39@gmail.com>
2024-03-22 16:35:51 +0100

    second try

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # get_next_line
-42 project
+# 42 project
+//this project is going to make a function called get_next_line which is going to print the next line after '\n'
+Its made up by 3 functions which are connected tegether to provide its functionality
+Here are the functions:
+    1.ft_read_from_fd
+    2._extract_line
+
+1.*Ft_Read_from_fd*
+-This helper function reads data from the file descriptor 'fd'into the provided buffer , up to the specified size size.
+_It checks for the situation wher read is return -1 or 0 and return NULL
+_it allocates memmory for the new string to which we are going to put the words from buffer up to bytes_read.
+_Then it copies to the data  to the new string and null terminate the string
+
+2.*ft_extract_line*
+-
--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_read.c                                          :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkwizera <mkwizera@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kuzi <kuzi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 07:55:17 by mkwizera          #+#    #+#             */
-/*   Updated: 2024/03/20 09:13:44 by mkwizera         ###   ########.fr       */
+/*   Updated: 2024/03/22 16:35:01 by kuzi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,17 @@ char	*ft_read_from_fd(int fd)
 // ngiye noneho guhita nkora line extractin ninyirangiza 
 // igomba kuza kuritaninga new linendibuze guikoresh mur
 // get next line 
+char *ft_exctract_line(char *read_data,char *left0ver, int leftoversize)
+{
+	int read_data_size;
+
+	read_data_size = ft_strlen(read_data);
+	// ibintu byo gukora aha nuko ndi bunyure muri readdata nshake new line 
+	// noneho aho nyibonera nyireturning noneho mpite na updating leftover
+	// nshyiramo amagambo asigaye nakuyeho abanza.
+	// niba ari yo yanyuma yar isigaye mpite nkora break.
+	while (*read_data != '\n')
+	{
+		
+	}
+}
